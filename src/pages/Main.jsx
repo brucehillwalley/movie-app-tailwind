c3772d781a9358fe231f70c1a9075aa8388fe4ff
BruceHill <brucehillwalley@gmail.com>
2023-12-28 23:05:38 +0300

    search

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,11 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import { useMovieContext } from "../context/MovieContext";
 import MovieCard from "../components/MovieCard";
 
 const Main = () => {
-  const { movies, loading } = useMovieContext();
+  const { movies, loading, getMovies } = useMovieContext();
+  const [search, setSearch] = useState("");
+  const API_KEY = process.env.REACT_APP_TMDB_KEY;
+  const SEARCH_API = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${search}`;
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    getMovies(SEARCH_API);
+  }
   return (
+
     <div>
+          <form className="flex justify-center p-2" onSubmit={handleSubmit}>
+        <input
+          type="search"
+          className="w-80 h-8 rounded-md p-1 m-2"
+          placeholder="Search a movie..."
+          onChange={(e) => setSearch(e.target.value)}
+
+        />
+        <button className="btn-danger-bordered" type="submit">
+          Search
+        </button>
+      </form>
       <div className="flex flex-wrap justify-center">
         {loading ? (
           <div
